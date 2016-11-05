c9820a3454b25473e83333b89e807790d3aea7dd
Tiago Cruzeiro <tiago.c.1991@gmail.com>
2016-11-05 03:29:03 +0000

    Minor tweaks

--- Web Runner/Assets/Scripts/PlayerController.cs
+++ Web Runner/Assets/Scripts/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour {
                 LeftRight = speed;
         }
 
-        LeftRight -= Input.acceleration.x * 4;
+        LeftRight -= Input.acceleration.x * 6;
 
         //Vector3 Movement = new Vector3(LeftRight, 0, 0);
         //rb.AddForce(Movement * speed);
--- Web Runner/Assets/Scripts/SmoothCamera.cs
+++ Web Runner/Assets/Scripts/SmoothCamera.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class SmoothCamera : MonoBehaviour {
 
-    public GameObject player; //assign this in the editor or find it in the start function.
-    private Vector3 _velocity = Vector3.zero;
+    public GameObject player;
+    private Vector3 speed = Vector3.zero;
+    public float smoothTime = 0.9f;
 
     private void LateUpdate()
     {
-        this.gameObject.transform.position = Vector3.SmoothDamp(this.gameObject.transform.position, Camera.main.transform.position, ref _velocity, 0.5f);
-        //change the last value to the time you want to complete the movement.
+        Vector3 newPos = Vector3.SmoothDamp(this.gameObject.transform.position, Camera.main.transform.position, ref speed, smoothTime);
+        transform.position = newPos;
     }
 }
