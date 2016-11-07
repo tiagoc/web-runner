8dbda26736a50409f4b8c2edcf9a52f5a2c57f9f
gitinho <thug6757@gmail.com>
2016-11-07 15:41:27 +0000

    added missing textures

---
+++ Web Runner/Assets/Scripts/PowerUpRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpRotation : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    private float startTime;
+
+
+    void Start()
+    {
+        startTime = Time.time;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.transform.Translate(0, 0, Mathf.Sin((Time.time - startTime) * 1.5f *speed) / 10);
+        rb.transform.Rotate(0, 0, speed);
+    }
+}
